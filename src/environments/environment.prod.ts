18fed1044db692a4e4ebf068245ebdd4d8deeb2e
Min Naing Oo <min9wuu@gmail.com>
2022-02-06 22:29:34 +0630

    production bug on small screen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   firebaseUrl:
-    'https://profitolio-default-rtdb.asia-southeast1.firebasedatabase.app/',  
+    'https://profitolio-default-rtdb.asia-southeast1.firebasedatabase.app',
+  coingeckoBaseApiUrl: 'https://api.coingecko.com/api/v3',
+  coingeckoLatestPrices: '/simple/price',
+  coingeckoCoinList: '/coins/list',
 };
